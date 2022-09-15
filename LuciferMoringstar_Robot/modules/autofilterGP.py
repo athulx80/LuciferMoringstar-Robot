13013b0705987e4c9b7c3dfe9260d0260ed72457
Athul <103881131+athulx80@users.noreply.github.com>
2022-09-15 16:29:33 +0530

    Update autofilterGP.py

--- LuciferMoringstar_Robot/modules/autofilterGP.py
+++ LuciferMoringstar_Robot/modules/autofilterGP.py
@@ -44,7 +44,7 @@ async def group_filters(client, update):
             if settings["spellmode"]:
                 try:
                     reply = search.replace(" ", '+')  
-                    buttons = [[ InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}") ],[ InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close") ]]
+                    buttons = [[ InlineKeyboardButton("🔍 ꜱᴇᴀʀᴄʜ ᴛᴏ ɢᴏᴏɢʟᴇ", url=f"https://www.google.com/search?q={reply}") ],[ InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close") ]]
                     spell = await update.reply_text(text=settings["spelltext"].format(query=search, first_name=update.from_user.first_name, last_name=update.from_user.last_name, title=update.chat.title, mention=update.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))           
                     await asyncio.sleep(60)
                     await spell.delete()
@@ -77,10 +77,10 @@ async def group_filters(client, update):
             }
         else:
             buttons = btn
-            buttons.append([InlineKeyboardButton("📃 Pages 1/1",callback_data="pages"),
-                            InlineKeyboardButton("Close 🗑️", callback_data="close")])
+            buttons.append([InlineKeyboardButton("📃 PAGES 1/1",callback_data="pages"),
+                            InlineKeyboardButton("CLOSE 🗑️", callback_data="close")])
 
-            buttons.append([InlineKeyboardButton("🤖 𝙲𝙷𝙴𝙲𝙺 𝙼𝚈 𝙿𝙼 🤖", url=f"https://telegram.dog/{temp.Bot_Username}?")])
+            buttons.append([InlineKeyboardButton("🤖 ᴄʜᴇᴄᴋ ᴍʏ ᴘᴍ", url=f"https://telegram.dog/{temp.Bot_Username}?")])
 
             try:             
                 if settings["photo"]:
@@ -114,7 +114,7 @@ async def group_filters(client, update):
                         InlineKeyboardButton("🗑️", callback_data="close"),
                         InlineKeyboardButton("➡",callback_data=f"nextgroup_0_{keyword}")])
 
-        buttons.append([InlineKeyboardButton("🤖 𝙲𝙷𝙴𝙲𝙺 𝙼𝚈 𝙿𝙼 🤖", url=f"https://telegram.dog/{temp.Bot_Username}")])
+        buttons.append([InlineKeyboardButton("🤖 ᴄʜᴇᴄᴋ ᴍʏ ᴘᴍ", url=f"https://telegram.dog/{temp.Bot_Username}")])
 
         try:             
             if settings["photo"]:
